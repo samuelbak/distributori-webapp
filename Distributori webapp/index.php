3901d40222fd8129f307107f1c29a04c6b1207f7
sbacchetta <sbacchetta@PCCS6.isp.arvedi.it>
2017-04-09 13:41:31 +0200

    Map

--- Distributori webapp/index.php
+++ Distributori webapp/index.php
@@ -1,1 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+       #map {
+        height: 400px;
+        width: 100%;
+       }
+    </style>
+  </head>
+  <body>
+    <h3>My Google Maps Demo</h3>
+    <div id="map"></div>
+    <script>
+      function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAI6npnOVX6Y1j2aUKN5Ajh0crVze5B7G8&callback=initMap">
+    </script>
+  </body>
+</html>
